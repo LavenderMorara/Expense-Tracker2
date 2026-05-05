dfe16bea1b7d6c7574a1a1a09ef6fc0cb9a4748c
Deng26-tech <ajang.deng@students.jkuat.ac.ke>
2026-05-05 19:09:35 +0300

    Create AuthenticationException.java

---
+++ src/main/java/com/finance/tracker/exception/AuthenticationException.java
@@ -0,0 +1,8 @@
+package com.finance.tracker.exception;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
