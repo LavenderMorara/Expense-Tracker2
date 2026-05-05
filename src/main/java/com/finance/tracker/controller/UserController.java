f273e802c3f998343090105f08436c87eccdb768
Phantom-D1 <araxman878@gmail.com>
2026-05-05 22:28:08 +0300

    Update UserController.java

---
+++ src/main/java/com/finance/tracker/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.finance.tracker.controller;
+
+import com.finance.tracker.model.User;
+import com.finance.tracker.service.UserService;
+
+/**
+ * FIXED VERSION:
+ * - No broken wiring
+ * - Ensures UserService is initialized with logged-in user
+ * - Prevents null service issues in ProfilePanel
+ */
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(User loggedInUser) {
+        // IMPORTANT: service MUST know the logged-in user
+        this.userService = new UserService(loggedInUser);
+    }
+
+    public User getLoggedInUser() {
+        return userService.getLoggedInUser();
+    }
+
+    public boolean updateUsername(String newUsername) {
+        return userService.updateUsername(newUsername);
+    }
+
+    public boolean updatePassword(String oldPassword, String newPassword) {
+        return userService.updatePassword(oldPassword, newPassword);
+    }
+}
