170931af370dfc7f23c58cc8552fe2901ab49f3e
MataraKevin90 <133140200+MataraKevin90@users.noreply.github.com>
2026-05-07 19:22:35 +0300

    Update LoginRequestDTO.java

---
+++ src/main/java/com/finance/tracker/dto/LoginRequestDTO.java
@@ -0,0 +1,20 @@
+package com.finance.tracker.dto;
+
+public class LoginRequestDTO {
+
+    private String username;
+    private String password;
+
+    public LoginRequestDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
